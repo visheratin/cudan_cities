435da58e2a5fcf049292c21f5ee28de97dc0d5cf
Ksenia Mukhina <mukhinaks@users.noreply.github.com>
2020-12-18 15:18:48 +0200

    requirements updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ Created on Mon Dec 14 11:06:08 2020
 
 @author: Ksenia Mukhina
 """
-import numpy as np
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
+import datetime
+from dateutil.relativedelta import relativedelta
 
 folder = 'data/'   
 cities = {'London, UK': 'london', 
@@ -15,7 +16,27 @@ cities = {'London, UK': 'london',
       'Vienna, Austria': 'wienna', 
       'New York City, USA': 'new-york', } #'tokyo'
 
+coordinates = {'london':(51.507222, -0.1275), 
+          'spb':(59.9375, 30.308611),
+          'wienna':(48.2, 16.366667), 
+          'new-york':(40.71274, -74.005974), }
 
+def hex_to_rgb(color_string):
+    h = color_string.lstrip('#')
+    rgb_color = [int(h[i:i+2], 16) for i in (0, 2, 4)]
+    return rgb_color
+
+def date_range():
+    start = datetime.datetime(2017, 1, 1)
+    end = datetime.datetime(2020, 1, 1)
+    
+    rang = [start]
+    while start < end:
+        start += relativedelta(months=1)
+        rang.append(start)
+        
+    return rang
+          
 @st.cache
 def load_data(city):
     map_data = pd.read_csv(folder + city + '-filtered.csv')
@@ -43,7 +64,7 @@ def main_window(area):
         c = {}
         c['City'] = city
         c['Instagram locations'] = len(map_data)
-        c['Posts since 2016'] = sum(map_data.iloc[:, 6:].sum(axis=1))
+        c['Posts since 2017'] = sum(map_data.iloc[:, 6:].sum(axis=1))
         stats.append(c)
         
     df = pd.DataFrame(stats)
@@ -62,36 +83,50 @@ def main_window(area):
         
         st.map(load_data(cities[option]))
 
-def complex_data():
-     df = pd.DataFrame(
-         np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-         columns=['lat', 'lon'])
+def filter_data(map_data, date, value = 0):
+    tmp = map_data[map_data[date] > value]
+    return tmp[['lon','lat', date]]
+    
+def complex_data():     
+     option = st.selectbox(
+            'Select city',
+             list(cities.keys()), key=1)
      
+     map_data = load_data(cities[option])
+     
+     sl_date = st.select_slider('', date_range())
 
+     date = str(sl_date.year) + '-' + str(sl_date.month)
+     
      st.pydeck_chart(pdk.Deck(
          map_style='mapbox://styles/mapbox/light-v9',
          initial_view_state=pdk.ViewState(
-             latitude=37.76,
-             longitude=-122.4,
-             zoom=11,
-             pitch=50,
+             latitude=coordinates[cities[option]][0],
+             longitude=coordinates[cities[option]][1],
+             zoom=10,
+             pitch=0,
          ),
          layers=[
              pdk.Layer(
                 'HexagonLayer',
-                data=df,
+                data= filter_data(map_data, date, 5),
                get_position='[lon, lat]',
                 radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
+                elevation_scale=0,
                 pickable=True,
-                extruded=True,
+                extruded=False,
+                color_aggregation="SUM",
+                get_color_weight=date,
+                color_range=[[241,238,246],[212,185,218],[201,148,199],
+                             [223,101,176],[221,28,119],[152,0,67]]
+
              ),
     
          ],
      ))
-  #  
-      #  st.slider('', min_value=0, max_value=10)
-        
+                    #    get_fill_color=[255,0,0],
+                #"[255, (1 -" + date + "/" + str(map_data[date].max()) + ") * 255, 0]",
+              #  get_line_color=[255, 255, 255],
+             #   line_width_min_pixels=2,
 if __name__ == "__main__":
     main()
